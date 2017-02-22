e6b1d9da38fffc34c6083db59b6d2a23856547da
Samarth Gupta <samarthgupta1011@gmail.com>
2017-02-23 00:16:36 +0530

    New activities added - static

---
+++ app/src/main/java/com/halanx/tript/userapp/BecomeShopperActivity.java
@@ -0,0 +1,13 @@
+package com.halanx.tript.userapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BecomeShopperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_become_shopper);
+    }
+}
